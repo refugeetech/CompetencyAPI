15047030718ed457acce7ebabea9f7638c9ba72d
Radu Achim <radu.achim@iteam.se>
2016-02-06 16:16:34 +0100

    updated tests

--- test/routes/users.js
+++ test/routes/users.js
@@ -8,23 +8,62 @@ oboy((expect, sinon, proxyquire) => {
   let req
   let res
   let next
-
+  let elastic
 
   describe('/routes/users', _ => {
     beforeEach(() => {
       next = sinon.spy()
+
       req = {
         body: {}
       }
-      res = {}
 
-      users = proxyquire(process.cwd() + '/lib/routes/users', {})
+      res = {
+        send: sinon.spy()
+      }
+
+      elastic = {
+        save: sinon.stub(),
+        '@noCallThru': true
+      }
+
+      elastic.save.resolves(123456)
+
+      users = proxyquire(process.cwd() + '/lib/routes/users', {
+        '../adapters/elastic': elastic
+      })
     })
 
     it('rejects if we do not have a phone number', () => {
       users.create(req, res, next)
 
       expect(next).calledWith(new restifyErrors.BadRequestError('Missing Phone Number'))
+    })
+
+    it('saves the user to elastic', () => {
+
+      req.body = {
+        phoneNumber: '0707100100'
+      }
+
+      users.create(req)
+
+      expect(elastic.save).calledWith({
+        phoneNumber: '0707100100'
+      })
+    })
+
+    it('sends back the userId and calls next after', () => {
+      req.body = {
+        phoneNumber: '0707100100'
+      }
+
+      return users
+        .create(req, res, next)
+        .then(() => {
+          expect(res.send, 'res.send').calledWith(123456)
+          expect(next).calledWith()
+        })
     })
   })
 })
